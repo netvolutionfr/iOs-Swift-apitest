9b7f2af718d95de1280ee627456c5d23a5f8784b
Stanis Humez <stanis@netvolution.fr>
2023-03-21 14:50:22 +0100

    Initial Commit

---
+++ apitest/apitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  apitestApp.swift
+//  apitest
+//
+//  Created by Stanis Humez on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct apitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
